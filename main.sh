a2455378f668470508f3f5138ad09193524fa9b9
zeviel <ffxckemweball@gmail.com>
2026-07-21 19:05:42 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,8 @@
-docker run -d -p 8443:443 --name=tg-proxy --restart=always \
+docker run -d \
+  -p 8443:443 \
+  --name=tg-proxy \
+  --restart=always \
+  -e INTERNAL_IP="127.0.0.1" \
   -e SECRET="c741a811908c5b4238dee60fc14c784c" \
   -e TAG="b62807b6682914bcbd6ef432b20b89f4" \
   -e TLS_DOMAIN="web.yota.ru" \
