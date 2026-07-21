59a8c59c7955ba444f49952fcd77f373d00cbd29
zeviel <ffxckemweball@gmail.com>
2026-07-21 15:55:59 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,11 +1,18 @@
-docker rm -f mtproto-proxy-8443 2>/dev/null
+docker stop mtproto-proxy-8443 && docker rm mtproto-proxy-8443
 
+# Сгенерировать секрет с web.yota.ru
+SECRET=$(docker run --rm nineseconds/mtg:2 generate-secret web.yota.ru)
+
+sudo tee /etc/mtg-proxy/config.toml > /dev/null <<EOF
+secret = "${SECRET}"
+bind-to = "0.0.0.0:3128"
+ad-tag = "b73eb664e3dd95631c0b2112643d28d8"
+EOF
 
 sudo docker run -d \
     --name=mtproto-proxy-8443 \
     --restart=always \
     -p 8443:3128 \
-    -e MTG_SECRET=ff9f8d0be0a0eddb9fa2ad8d4b1ec8b8 \
-    -e MTG_AD_TAG="b73eb664e3dd95631c0b2112643d28d8" \
-    -e MTG_BIND_TO="0.0.0.0:3128" \
-    nineseconds/mtg:2
+    -v /etc/mtg-proxy:/config \
+    nineseconds/mtg:2 \
+    run /config/config.toml
