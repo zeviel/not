75f36085311edc27d409d6aff15fbc4a5040bf55
zeviel <ffxckemweball@gmail.com>
2026-07-21 19:39:38 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,11 @@
+docker stop mtproto-proxy-8443 && docker rm mtproto-proxy-8443
+
 docker run -d \
   --name mtproto-proxy-8443 \
   --restart=always \
   -p 8443:443 \
   -e SECRET="c741a811908c5b4238dee60fc14c784c" \
-  -e SECRET_HOST="web.yota.ru" \
+  -e SECRET_HOST="core.telegram.org" \
   -e TAG="b62807b6682914bcbd6ef432b20b89f4" \
   -e MTG_BIND_TO="0.0.0.0:443" \
   -e PUBLIC_IPV4="185.229.66.115" \
