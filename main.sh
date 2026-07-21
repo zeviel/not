81b9f5d64c5de5fed0af545cddcfa84f8c64b9c3
zeviel <ffxckemweball@gmail.com>
2026-07-21 16:57:11 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
-# 1. На всякий случай удаляем старый контейнер, если он остался
+# 1. Удаляем прошлый контейнер
 docker rm -f mtproto-proxy-8443 2>/dev/null || true
 
-# 2. Запускаем классический прокси с полной поддержкой Ad Tag
+# 2. Запускаем официальный образ Telegram Messenger Proxy
 docker run -d \
   --name="mtproto-proxy-8443" \
   --restart=always \
@@ -9,4 +9,6 @@ docker run -d \
   -e SECRET="eec741a811908c5b4238dee60fc14c784c7765622e796f74612e7275" \
   -e TAG="b62807b6682914bcbd6ef432b20b89f4" \
   -e WORKERS=2 \
-  sergant210/telegram-mtproto-proxy:latest
+  telegrammessenger/proxy:latest
+
+docker logs mtproto-proxy-8443
