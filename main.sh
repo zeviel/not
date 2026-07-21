b377306db7ff862b468b7b4d095027970007ba43
zeviel <ffxckemweball@gmail.com>
2026-07-21 19:50:34 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,13 +1,17 @@
 docker stop mtproto-proxy-8443 && docker rm mtproto-proxy-8443
 
+sudo mkdir -p /etc/mtg-proxy
+sudo tee /etc/mtg-proxy/config.toml > /dev/null <<EOF
+secret = "eec741a811908c5b4238dee60fc14c784c7765622e796f74612e7275"
+bind-to = "0.0.0.0:443"
+ad-tag = "b62807b6682914bcbd6ef432b20b89f4"
+public-ipv4 = "185.229.66.115"
+EOF
+
 docker run -d \
   --name mtproto-proxy-8443 \
   --restart=always \
   -p 8443:443 \
-  -e SECRET="c741a811908c5b4238dee60fc14c784c" \
-  -e SECRET_HOST="web.yota.ru" \
-  -e AD_TAG="b62807b6682914bcbd6ef432b20b89f4" \
-  -e MTG_BIND_TO="0.0.0.0:443" \
-  -e IP_OVERRIDE="185.229.66.115" \
-  ghcr.io/mhsanaei/mtg-multi:latest
-
+  -v "/etc/mtg-proxy:/config:ro" \
+  ghcr.io/mhsanaei/mtg-multi:latest \
+  run /config/config.toml
