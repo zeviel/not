eb64be16b307961ca11fc291789681c7cc895fda
zeviel <ffxckemweball@gmail.com>
2026-07-21 15:50:17 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ docker rm -f mtproto-proxy-8443 2>/dev/null
 sudo docker run -d \
     --name=mtproto-proxy-8443 \
     --restart=always \
-    -p 8443:443 \
+    -p 8443:3128 \
     -e MTG_SECRET=ff9f8d0be0a0eddb9fa2ad8d4b1ec8b8 \
     -e MTG_AD_TAG="b73eb664e3dd95631c0b2112643d28d8" \
     -e MTG_BIND_TO="0.0.0.0:3128" \
