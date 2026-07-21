42380254aeceef022afc99a09d84b44e13f22c79
zeviel <ffxckemweball@gmail.com>
2026-07-21 18:11:37 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,16 +1,14 @@
-# 1. Очищаем старый контейнер
-docker rm -f mtproto-proxy-8443 2>/dev/null || true
+# 1. Меняем рекламный токен в конфигурационном файле на сервере
+sudo sed -i 's/ad-tag = .*/ad-tag = "b62807b6682914bcbd6ef432b20b89f4"/' /etc/mtg-proxy/config.toml
+cat /etc/mtg-proxy/config.toml
+# 2. Удаляем старый контейнер
+docker rm -f mtproto-proxy-8443
 
-# 2. Запускаем mtg:1 с правильным позиционным синтаксисом
-docker run -d \
-  --name="mtproto-proxy-8443" \
-  --restart=always \
-  -p "8443:3128" \
-  nineseconds/mtg:1 \
-  run \
-  "eec741a811908c5b4238dee60fc14c784c7765622e796f74612e7275" \
-  "b62807b6682914bcbd6ef432b20b89f4"
-
-
-
-docker logs mtproto-proxy-8443
+# 3. Запускаем заново с обновленным конфигом
+sudo docker run -d \
+    --name="mtproto-proxy-8443" \
+    --restart=always \
+    -p "8443:3128" \
+    -v "/etc/mtg-proxy:/config:ro" \
+    nineseconds/mtg:2 \
+    run /config/config.toml
