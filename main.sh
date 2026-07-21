3442fb8165559d487ab58652045cd8e3f0cc69cb
zeviel <ffxckemweball@gmail.com>
2026-07-21 16:51:40 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,14 +1,17 @@
-# 1. Меняем рекламный токен в конфигурационном файле на сервере
-sudo sed -i 's/ad-tag = .*/ad-tag = "b62807b6682914bcbd6ef432b20b89f4"/' /etc/mtg-proxy/config.toml
-cat /etc/mtg-proxy/config.toml
-# 2. Удаляем старый контейнер
 docker rm -f mtproto-proxy-8443
+sudo tee /etc/mtg-proxy/config.toml > /dev/null <<EOF
+[[secret]]
+secret = "eec741a811908c5b4238dee60fc14c784c7765622e796f74612e7275"
+ad-tag = "b62807b6682914bcbd6ef432b20b89f4"
 
-# 3. Запускаем заново с обновленным конфигом
+bind-to = "0.0.0.0:3128"
+EOF
+
+cat /etc/mtg-proxy/config.toml
 sudo docker run -d \
     --name="mtproto-proxy-8443" \
     --restart=always \
     -p "8443:3128" \
     -v "/etc/mtg-proxy:/config:ro" \
-    nineseconds/mtg:2 \
+    devgsc/mtg-multi:latest \
     run /config/config.toml
