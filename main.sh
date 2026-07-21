28462d26730b2e0834dce3be4b1222929a531da7
zeviel <ffxckemweball@gmail.com>
2026-07-21 19:07:45 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 docker run -d \
-  -p 8443:443 \
-  --name=tg-proxy \
+  --name mtproto-proxy-8443 \
   --restart=always \
-  -e INTERNAL_IP="127.0.0.1" \
+  -p 8443:443 \
   -e SECRET="c741a811908c5b4238dee60fc14c784c" \
+  -e SECRET_HOST="web.yota.ru" \
   -e TAG="b62807b6682914bcbd6ef432b20b89f4" \
-  -e TLS_DOMAIN="web.yota.ru" \
-  telegrammessenger/proxy:2.0beta
+  -e MTG_BIND_TO="0.0.0.0:443" \
+  ghcr.io/mhsanaei/mtg-multi:latest
