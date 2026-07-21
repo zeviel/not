c9f337266289c84970576709e8ebfa0bd3f301be
zeviel <ffxckemweball@gmail.com>
2026-07-21 15:46:28 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,11 @@
 docker rm -f mtproto-proxy-8443 2>/dev/null
 
-sudo docker run -d --name=mtproto-proxy-8443 --restart=always -p 0.0.0.0:8443:8443 -e MPROXY_PORT=8443 -v proxy-config-8443:/data -e SECRET=ff9f8d0be0a0eddb9fa2ad8d4b1ec8b8 -e TAG="b73eb664e3dd95631c0b2112643d32d8" telegrammessenger/proxy:latest
+
+sudo docker run -d \
+    --name=mtproto-proxy-8443 \
+    --restart=always \
+    -p 8443:443 \
+    -e MTG_SECRET=ff9f8d0be0a0eddb9fa2ad8d4b1ec8b8 \
+    -e MTG_AD_TAG="b73eb664e3dd95631c0b2112643d28d8" \
+    -e MTG_BIND_TO="0.0.0.0:3128" \
+    nineseconds/mtg:2
