522d1851f290f5cda191d9c22ba4f2c0f68499a4
zeviel <ffxckemweball@gmail.com>
2026-07-21 19:46:41 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,8 +6,8 @@ docker run -d \
   -p 8443:443 \
   -e SECRET="c741a811908c5b4238dee60fc14c784c" \
   -e SECRET_HOST="web.yota.ru" \
-  -e TAG="b62807b6682914bcbd6ef432b20b89f4" \
+  -e AD_TAG="b62807b6682914bcbd6ef432b20b89f4" \
   -e MTG_BIND_TO="0.0.0.0:443" \
-  -e PUBLIC_IPV4="185.229.66.115" \
   -e IP_OVERRIDE="185.229.66.115" \
   ghcr.io/mhsanaei/mtg-multi:latest
+
