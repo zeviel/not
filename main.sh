cc1a3874d657491d3ae42e214fe942a4d468edaa
zeviel <ffxckemweball@gmail.com>
2026-07-21 16:55:32 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,17 +1,12 @@
-docker rm -f mtproto-proxy-8443
-sudo tee /etc/mtg-proxy/config.toml > /dev/null <<EOF
-[[secret]]
-secret = "eec741a811908c5b4238dee60fc14c784c7765622e796f74612e7275"
-ad-tag = "b62807b6682914bcbd6ef432b20b89f4"
+# 1. На всякий случай удаляем старый контейнер, если он остался
+docker rm -f mtproto-proxy-8443 2>/dev/null || true
 
-bind-to = "0.0.0.0:3128"
-EOF
-
-cat /etc/mtg-proxy/config.toml
-sudo docker run -d \
-    --name="mtproto-proxy-8443" \
-    --restart=always \
-    -p "8443:3128" \
-    -v "/etc/mtg-proxy:/config:ro" \
-    dolonet/mtg-multi:latest \
-    run /config/config.toml
+# 2. Запускаем классический прокси с полной поддержкой Ad Tag
+docker run -d \
+  --name="mtproto-proxy-8443" \
+  --restart=always \
+  -p "8443:443" \
+  -e SECRET="eec741a811908c5b4238dee60fc14c784c7765622e796f74612e7275" \
+  -e TAG="b62807b6682914bcbd6ef432b20b89f4" \
+  -e WORKERS=2 \
+  sergant210/telegram-mtproto-proxy:latest
