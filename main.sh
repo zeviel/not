4447503affdac1e53d3d2c9f903c46182e21c8f1
zeviel <ffxckemweball@gmail.com>
2026-07-28 20:16:31 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,13 +1,13 @@
 docker rm -f mtproto-proxy mtproto-proxy-2 2>/dev/null || true
 
-# 1. Запуск первого прокси на 443 порту
+# 1. Первый контейнер на 443 порту (Секрет изменен на валидный HEX)
 docker run -d \
   --name=mtproto-proxy \
   --restart=always \
   -p 443:3128 \
-  nineseconds/mtg:2 simple-run 0.0.0.0:3128 ee7765622e796f74612e72755b744f13 -a 8b65a4af31191c0e4f9e64c44f0d3d1e
+  nineseconds/mtg:2 simple-run 0.0.0.0:3128 ee7765622e796f74612e72755b744f12 -a 8b65a4af31191c0e4f9e64c44f0d3d1e
 
-# 2. Запуск второго прокси на 8443 порту
+# 2. Второй контейнер на 8443 порту (Ваш оригинальный рабочий секрет c741...)
 docker run -d \
   --name=mtproto-proxy-2 \
   --restart=always \
