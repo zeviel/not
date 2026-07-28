1b65bca298413a392b95252be2f6cfe9c72a9dec
zeviel <ffxckemweball@gmail.com>
2026-07-28 19:33:06 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,4 +3,4 @@ docker run -d -p 8443:443 --name=tg-proxy --restart=always \
   -e SECRET=c741a811908c5b4238dee60fc14c784c \
   -e TAG=b62807b6682914bcbd6ef432b20b89f4 \
   telegrammessenger/proxy:latest \
-  /bin/sh -c "/run.sh -M 10000"
+  /bin/bash -c "sed -i 's/maxconns 60000/maxconns 2000/g' /run.sh && /run.sh"
