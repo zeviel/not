e3e5ab16d6ad74bfdaa12a38d56686e3871775cd
zeviel <ffxckemweball@gmail.com>
2026-07-28 19:26:32 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,5 @@
 docker rm -f tg-proxy
 docker run -d -p 8443:8443 --name=tg-proxy --restart=always \
-  --sysctl net.ipv4.ip_local_port_range="1024 65000" \
   --dns 1.1.1.1 \
   -e MTP_PORT=8443 \
   -e MTP_SECRET="c741a811908c5b4238dee60fc14c784c" \
