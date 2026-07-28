723ec16184ab292bbb2ab2a355133cd5b3e451e1
zeviel <ffxckemweball@gmail.com>
2026-07-28 19:09:25 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,6 @@
 docker rm -f tg-proxy
-docker run -d -p 8443:8443 --name=tg-proxy --restart=always \
-  --sysctl net.ipv6.conf.all.disable_ipv6=1 \
-  --dns 8.8.8.8 \
-  -e MTP_PORT=8443 \
-  -e MTP_SECRET="c741a811908c5b4238dee60fc14c784c" \
-  -e MTP_TAG="b62807b6682914bcbd6ef432b20b89f4" \
-  seriyps/mtproto-proxy:latest
+docker run -d -p 8443:443 --name=tg-proxy --restart=always \
+  -e SECRET=c741a811908c5b4238dee60fc14c784c \
+  -e TAG=b62807b6682914bcbd6ef432b20b89f4 \
+  telegrammessenger/proxy:latest
 
