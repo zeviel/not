2239a3975c6311c775f2dd9f801b3652b629abc7
zeviel <ffxckemweball@gmail.com>
2026-07-28 21:47:38 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -26,7 +26,7 @@ docker run -d \
   -p 443:443 \
   -v mtproto-proxy-config:/data \
   -e SECRET=ee7765622e796f74612e72755b744f13 \
-  -e TAG=8b654af431191c0e4f9e64c44f0d3d1e \
+  -e TAG=8b65a4af31191c0e4f9e64c44f0d3d1e \
   -e WORKERS=2 \
   telegrammessenger/proxy:latest
 
