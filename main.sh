2cf6e0aa2cbab9e244107ecde4edcb2fd9e0d387
zeviel <ffxckemweball@gmail.com>
2026-07-28 19:49:49 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,8 @@
-docker run -d -p 443:443 --name=mtproto-proxy --restart=always \
-  -e SECRET=ee7765622e796f74612e72755b744f13 \
-  -e TAG=8b65a4af31191c0e4f9e64c44f0d3d1e \
-  telegrammessenger/proxy:latest
-
+docker run -d \
+    --name=mtproto-proxy \
+    --restart=unless-stopped \
+    -p 443:443 \
+    -v proxy-config-443:/data \
+    -e SECRET=ee7765622e796f74612e72755b744f13 \
+    -e TAG=8b65a4af31191c0e4f9e64c44f0d3d1e \
+    telegrammessenger/proxy:latest
