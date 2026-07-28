cb1b0652712f376a96015728c211ae1f5ea120b2
zeviel <ffxckemweball@gmail.com>
2026-07-28 19:35:52 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,11 @@
 docker rm -f tg-proxy
-docker run -d -p 8443:443 --name=tg-proxy --restart=always \
-  -e SECRET=c741a811908c5b4238dee60fc14c784c \
-  -e TAG=b62807b6682914bcbd6ef432b20b89f4 \
-  telegrammessenger/proxy:latest \
-  /bin/bash -c "sed -i 's/maxconns 60000/maxconns 2000/g' /run.sh && /run.sh"
+apt-get update && apt-get install -y git curl build-essential libssl-dev zlib1g-dev
+git clone https://github.com/TelegramMessenger/MTProxy
+cd MTProxy
+make && cd objs/bin
+curl -s https://core.telegram.org/getProxySecret -o proxy-secret
+curl -s https://core.telegram.org/getProxyConfig -o proxy-multi.conf
+cd objs/bin && ./mtproto-proxy -u nobody -p 8888 -H 8443 \
+  -S c741a811908c5b4238dee60fc14c784c \
+  -P b62807b6682914bcbd6ef432b20b89f4 \
+  --aes-pwd proxy-secret proxy-multi.conf -M 1 &
