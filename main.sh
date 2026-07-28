2298c30a51ab201c4384deefb076098a9720f14f
zeviel <ffxckemweball@gmail.com>
2026-07-28 19:03:17 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,6 @@
-
-docker run -d -p 8443:8443 --name=tg-proxy --restart=always \
+docker rm -f tg-proxy
+docker run -d --network host --name=tg-proxy --restart=always \
   -e MTP_PORT=8443 \
   -e MTP_SECRET="c741a811908c5b4238dee60fc14c784c" \
   -e MTP_TAG="b62807b6682914bcbd6ef432b20b89f4" \
-  --dns 8.8.8.8 --dns 1.1.1.1 \
   seriyps/mtproto-proxy:latest
