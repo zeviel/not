73771f13ed4b68a90b54d63f4db7531ed570e9f7
zeviel <ffxckemweball@gmail.com>
2026-07-28 20:18:27 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,15 +1,19 @@
 docker rm -f mtproto-proxy mtproto-proxy-2 2>/dev/null || true
 
-# 1. Первый контейнер на 443 порту (Секрет изменен на валидный HEX)
 docker run -d \
   --name=mtproto-proxy \
   --restart=always \
-  -p 443:3128 \
-  nineseconds/mtg:2 simple-run 0.0.0.0:3128 ee7765622e796f74612e72755b744f12 -a 8b65a4af31191c0e4f9e64c44f0d3d1e
+  -p 443:443 \
+  -e SECRET=ee7765622e796f74612e72755b744f13 \
+  -e TAG=8b65a4af31191c0e4f9e64c44f0d3d1e \
+  telegrammessenger/proxy:latest \
+  /bin/bash -c "exec /objs/bin/mtproto-proxy -u nobody -p 8888 -H 443 -S ee7765622e796f74612e72755b744f13 -P 8b65a4af31191c0e4f9e64c44f0d3d1e --aes-pwd /data/proxy-secret /data/proxy-multi.conf -M 1"
 
-# 2. Второй контейнер на 8443 порту (Ваш оригинальный рабочий секрет c741...)
 docker run -d \
   --name=mtproto-proxy-2 \
   --restart=always \
-  -p 8443:3128 \
-  nineseconds/mtg:2 simple-run 0.0.0.0:3128 c741a811908c5b4238dee60fc14c784c -a b62807b6682914bcbd6ef432b20b89f4
+  -p 8443:443 \
+  -e SECRET=c741a811908c5b4238dee60fc14c784c \
+  -e TAG=b62807b6682914bcbd6ef432b20b89f4 \
+  telegrammessenger/proxy:latest \
+  /bin/bash -c "exec /objs/bin/mtproto-proxy -u nobody -p 8888 -H 443 -S c741a811908c5b4238dee60fc14c784c -P b62807b6682914bcbd6ef432b20b89f4 --aes-pwd /data/proxy-secret /data/proxy-multi.conf -M 1"
