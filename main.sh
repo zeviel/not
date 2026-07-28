8bd93c718195d736605de9fa4c872c5f1113c695
zeviel <ffxckemweball@gmail.com>
2026-07-28 20:19:27 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
 docker rm -f mtproto-proxy mtproto-proxy-2 2>/dev/null || true
 
+# Контейнер 1 на порту 443
 docker run -d \
   --name=mtproto-proxy \
   --restart=always \
@@ -7,8 +8,9 @@ docker run -d \
   -e SECRET=ee7765622e796f74612e72755b744f13 \
   -e TAG=8b65a4af31191c0e4f9e64c44f0d3d1e \
   telegrammessenger/proxy:latest \
-  /bin/bash -c "exec /objs/bin/mtproto-proxy -u nobody -p 8888 -H 443 -S ee7765622e796f74612e72755b744f13 -P 8b65a4af31191c0e4f9e64c44f0d3d1e --aes-pwd /data/proxy-secret /data/proxy-multi.conf -M 1"
+  /bin/bash -c "sed -i 's/exec \/objs\/bin\/mtproto-proxy/exec \/objs\/bin\/mtproto-proxy -M 1/g' /run.sh && exec /run.sh"
 
+# Контейнер 2 на порту 8443
 docker run -d \
   --name=mtproto-proxy-2 \
   --restart=always \
@@ -16,4 +18,4 @@ docker run -d \
   -e SECRET=c741a811908c5b4238dee60fc14c784c \
   -e TAG=b62807b6682914bcbd6ef432b20b89f4 \
   telegrammessenger/proxy:latest \
-  /bin/bash -c "exec /objs/bin/mtproto-proxy -u nobody -p 8888 -H 443 -S c741a811908c5b4238dee60fc14c784c -P b62807b6682914bcbd6ef432b20b89f4 --aes-pwd /data/proxy-secret /data/proxy-multi.conf -M 1"
+  /bin/bash -c "sed -i 's/exec \/objs\/bin\/mtproto-proxy/exec \/objs\/bin\/mtproto-proxy -M 1/g' /run.sh && exec /run.sh"
