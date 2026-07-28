c64c2833eaf91f4d9042122b17bb429e4b1a69cc
zeviel <ffxckemweball@gmail.com>
2026-07-28 17:25:11 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
-docker run -d -p 8443:8443 --name=tg-proxy --restart=always \
-  -e SECRET="c741a811908c5b4238dee60fc14c784c" \
-  -e TAG="b62807b6682914bcbd6ef432b20b89f4" \
+
+docker run -d -p 8443:8443 --name=mtproto-proxy --restart=always \
   -e MTP_PORT=8443 \
+  -e MTP_SECRET="c741a811908c5b4238dee60fc14c784c" \
+  -e MTP_TAG="b62807b6682914bcbd6ef432b20b89f4" \
   seriyps/mtproto-proxy:latest
