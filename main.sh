4b5167ac7e99ed5b9a44ce7d4b756c890de3b177
zeviel <ffxckemweball@gmail.com>
2026-07-28 19:06:26 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
 docker rm -f tg-proxy
 docker run -d --network host --name=tg-proxy --restart=always \
+  -e ERL_FLAGS="-proto_dist inet_tcp" \
   -e MTP_PORT=8443 \
   -e MTP_SECRET="c741a811908c5b4238dee60fc14c784c" \
   -e MTP_TAG="b62807b6682914bcbd6ef432b20b89f4" \
