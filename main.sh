4c076158fa1d28c395f8b4b6b8fd5b74a510ef2c
zeviel <ffxckemweball@gmail.com>
2026-07-29 18:11:57 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,9 @@
 docker stop tg-proxy && docker rm tg-proxy
+
 docker run -d \
   --name tg-proxy \
   --restart=always \
   -p 8443:8443 \
   -v /etc/mtg-proxy:/config:ro \
-  ghcr.io/mhsanaei/mtg-multi:latest run /config/config.toml
+  nineseconds/mtg:latest \
+  /config/config.toml
