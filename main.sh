d603ba9e286bb02f1ba533b9aa79163b623b647d
zeviel <ffxckemweball@gmail.com>
2026-07-29 18:08:57 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
-
-echo "🚀 Запуск прокси 2 (порт 8443)..."
+docker stop tg-proxy && docker rm tg-proxy
 docker run -d \
-  --name=mtproto-proxy-2 \
+  --name tg-proxy \
   --restart=always \
-  --privileged \
-  --ulimit nofile=65536:65536 \
-  -p 8443:443 \
-  -v mtproto-proxy-config-2:/data \
-  -e SECRET=c741a811908c5b4238dee60fc14c784c \
-  -e TAG=b62807b6682914bcbd6ef432b20b89f4 \
-  -e WORKERS=2 \
-  telegrammessenger/proxy:latest
+  -p 8443:8443 \
+  ghcr.io/mhsanaei/mtg-multi:latest \
+  run \
+  --secret "eec741a811908c5b4238dee60fc14c784c7765622e796f74612e7275" \
+  --bind-to "[::]:8443" \
+  --public-ipv6 "2a0a:9300:1:1726::1" \
+  --public-ipv4 "185.229.66.115" \
+  --allow-fallback-on-unknown-dc \
+  --ad-tag "b62807b6682914bcbd6ef432b20b89f4"
