e85f5f0030baaa52fe0a6a33783a9a24202e47ce
zeviel <ffxckemweball@gmail.com>
2026-07-29 18:18:45 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,6 +5,6 @@ docker run -d \
   --restart=always \
   -p 8443:8443 \
   nineseconds/mtg:2 \
-  run -t "eec741a811908c5b4238dee60fc14c784c7765622e796f74612e7275" \
-  -a "185.229.66.115:8443" \
+  --secret "eec741a811908c5b4238dee60fc14c784c7765622e796f74612e7275" \
+  --addr "185.229.66.115:8443" \
   --adtag "b62807b6682914bcbd6ef432b20b89f4"
