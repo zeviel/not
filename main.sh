ff5a795a8a26283b2e50b32d83523ecdb3b7dee8
zeviel <ffxckemweball@gmail.com>
2026-07-29 18:21:26 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,18 @@
 docker stop tg-proxy && docker rm tg-proxy
 
+cat > /etc/mtg-proxy/config.toml <<'EOF'
+secret = "eec741a811908c5b4238dee60fc14c784c7765622e796f74612e7275"
+bind-to = "0.0.0.0:8443"
+ad-tag = "b62807b6682914bcbd6ef432b20b89f4"
+public-ipv4 = "185.229.66.115"
+allow-fallback-on-unknown-dc = true
+EOF
+
+
 docker run -d \
   --name tg-proxy \
   --restart=always \
   -p 8443:8443 \
-  nineseconds/mtg:2 \
-  --secret "eec741a811908c5b4238dee60fc14c784c7765622e796f74612e7275" \
-  --addr "185.229.66.115:8443" \
-  --adtag "b62807b6682914bcbd6ef432b20b89f4"
+  -v /etc/mtg-proxy:/config:ro \
+  ghcr.io/mhsanaei/mtg-multi:latest \
+  run /config/config.toml
