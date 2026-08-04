4b3bd709cf1d32bf51e5c8491263133edde0842f
zeviel <ffxckemweball@gmail.com>
2026-08-04 23:21:11 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,6 +6,6 @@ docker run -d --restart=always --name tg-proxy \
   -p 8443:443 \
   --privileged \
   --ulimit nofile=65535:65535 \
-  -e SECRET="eec741a811908c5b4238dee60fc14c784c7765622e796f74612e7275" \
+  -e SECRET="c741a811908c5b4238dee60fc14c784c" \
   -e TAG="b62807b6682914bcbde6f432b20b89f4" \
   telegrammessenger/proxy:latest
