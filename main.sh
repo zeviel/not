79fe4a9796c045442cba48076815cb2dd492ff9b
zeviel <ffxckemweball@gmail.com>
2026-08-05 00:49:33 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,2 @@
-wget -qO- https://github.com/telemt/telemt/releases/latest/download/install.sh | sh
+wget https://github.com/telemt/telemt/releases/download/3.4.25/telemt-x86_64-v3-linux-gnu.tar.gz
+tar -xzf telemt-x86_64-v3-linux-gnu.tar.gz
