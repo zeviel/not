a6dfa562fe4c17d49e27ac225f12edf7d1a58441
zeviel <ffxckemweball@gmail.com>
2026-08-05 00:48:01 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,16 +1,1 @@
-# Cloning repo
-git clone https://github.com/telemt/telemt 
-# Changing Directory to telemt
-cd telemt
-# Starting Release Build
-cargo build --release
-
-# Current release profile uses lto = "fat" for maximum optimization (see Cargo.toml).
-# On low-RAM systems (~1 GB) you can override it to "thin".
-
-# Move to /bin
-mv ./target/release/telemt /bin
-# Make executable
-chmod +x /bin/telemt
-# Lets go!
-telemt config.toml
+wget -qO- https://github.com/telemt/telemt/releases/latest/download/install.sh | sh
