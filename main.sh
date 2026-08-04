f1ea8b04866b382ad89efae4a3f8687c09a577fb
zeviel <ffxckemweball@gmail.com>
2026-08-05 01:26:08 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,5 +4,5 @@ docker run -d \
   --name tg-proxy \
   --restart=always \
   -p 8443:3128 \
-  -v /etc/mtg-proxy/config.toml:/config.toml:ro \
-  nineseconds/mtg:1
+  nineseconds/mtg:1 \
+  run --ip 185.229.66.115 eec741a811908c5b4238dee60fc14c784c7765622e796f74612e7275 b62807b6682914bcbd6ef432b20b89f4
