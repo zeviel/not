1f301744ffcccff4128bd061e023d2d8779c08e3
zeviel <ffxckemweball@gmail.com>
2026-08-04 23:33:08 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,14 @@
+# Останавливаем и удаляем падающий контейнер
 docker stop tg-proxy && docker rm -f tg-proxy
+
+# Создаем папку для данных на хосте
+mkdir -p ~/mtproxy_data
+
+# Запускаем контейнер с томом, правами на запись и чистым окружением
 docker run -d --restart=always --name tg-proxy \
   -p 8443:443 \
-  -e SECRET="c741a811908c5b4238dee60fc14c784c" \
+  -v ~/mtproxy_data:/data \
+  -e SECRET="eec741a811908c5b4238dee60fc14c784c7765622e796f74612e7275" \
   -e TAG="b62807b6682914bcbde6f432b20b89f4" \
   -e TLS_DOMAIN="web.yota.ru" \
-  c0rc0r4n/mtproto-proxy:latest
+  seriyps/mtproto-proxy:latest
