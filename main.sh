a3d124c2460450bcec2a8ccfd4ef858a03629906
zeviel <ffxckemweball@gmail.com>
2026-08-04 23:22:33 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,11 +1,9 @@
-
 docker stop tg-proxy && docker rm -f tg-proxy
 
-# Перезапускаем с переменной SECRET
 docker run -d --restart=always --name tg-proxy \
   -p 8443:443 \
-  --privileged \
   --ulimit nofile=65535:65535 \
   -e SECRET="c741a811908c5b4238dee60fc14c784c" \
   -e TAG="b62807b6682914bcbde6f432b20b89f4" \
+  -e TLS_DOMAIN="web.yota.ru" \
   telegrammessenger/proxy:latest
