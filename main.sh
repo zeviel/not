30e1bda73cbe4ca3e0e20953ef8d643c6e5b9259
zeviel <ffxckemweball@gmail.com>
2026-08-04 23:26:31 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -7,4 +7,4 @@ docker run -d --restart=always --name tg-proxy \
   -e SECRET="c741a811908c5b4238dee60fc14c784c" \
   -e TAG="b62807b6682914bcbde6f432b20b89f4" \
   -e TLS_DOMAIN="web.yota.ru" \
-  telegrammessenger/proxy:latest
+  seriyps/mtproto-proxy:latest
