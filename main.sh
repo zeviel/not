5d6111e2a306e85c044aa076e69eb0308d3ee025
zeviel <ffxckemweball@gmail.com>
2026-08-04 23:20:06 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,11 @@
-docker rm -f tg-proxy
 
+docker stop tg-proxy && docker rm -f tg-proxy
+
+# Перезапускаем с переменной SECRET
 docker run -d --restart=always --name tg-proxy \
   -p 8443:443 \
   --privileged \
   --ulimit nofile=65535:65535 \
-  -v /etc/telemt-config:/config \
+  -e SECRET="eec741a811908c5b4238dee60fc14c784c7765622e796f74612e7275" \
+  -e TAG="b62807b6682914bcbde6f432b20b89f4" \
   telegrammessenger/proxy:latest
