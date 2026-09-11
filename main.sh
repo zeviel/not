43f9aaebb42260995967473ea1845bc7ec53d48a
zeviel <ffxckemweball@gmail.com>
2026-09-11 18:03:47 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,20 +1,3 @@
-
-docker stop tg-proxy && docker rm tg-proxy
-
-cat > /etc/mtg-proxy/config.toml <<'EOF'
-secret = "eec741a811908c5b4238dee60fc14c784c7765622e796f74612e7275"
-bind-to = "0.0.0.0:8443"
-ad-tag = "b62807b6682914bcbd6ef432b20b89f4"
-public-ipv4 = "185.229.66.115"
-allow-fallback-on-unknown-dc = true
-auto-update = true
-EOF
-
-docker run -d \
-  --name tg-proxy \
-  --restart=always \
-  -p 8443:8443 \
-  --add-host=web.yota.ru:185.229.66.115 \
-  -v /etc/mtg-proxy:/config:ro \
-  ghcr.io/mhsanaei/mtg-multi:latest \
-  run /config/config.toml
+wget "https://github.com/kulikov0/whitelist-bypass/releases/download/v0.3.8/whitelist-bypass-cli-linux-x64.zip"
+unzip whitelist-bypass-cli-linux-x64.zip
+ls
