9947e0f6bb2e204aee9f86dc4ca322f8b222591d
zeviel <ffxckemweball@gmail.com>
2026-09-20 00:12:24 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,11 @@
-curl -fsSL https://raw.githubusercontent.com/openlibrecommunity/olcrtc/master/install.sh | bash
+git clone --depth 1 --recurse-submodules https://github.com/openlibrecommunity/olcrtc.git
+cd olcrtc
+
+podman run --rm \
+  --runtime=crun \
+  --ulimit nofile=65536:65536 \
+  --network host \
+  -v "$(pwd)":/app:Z \
+  -w /app \
+  docker.io/library/golang:1.26-alpine3.22 \
+  sh -c "go mod download && go build -trimpath -ldflags='-s -w' -o olcrtc ./cmd/olcrtc"
