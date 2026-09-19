c4b22613947bd4bb47d19c5ab503e413f2192e43
zeviel <ffxckemweball@gmail.com>
2026-09-19 22:13:51 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,4 @@
+echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_$(lsb_release -rs)/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_$(lsb_release -rs)/Release.key | sudo apt-key add -
+sudo apt install podman
 curl -fsSL https://raw.githubusercontent.com/openlibrecommunity/olcrtc/master/install.sh | bash
