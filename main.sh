58d8f1d404f7346b5ecbd8dd1e362034379efd8c
zeviel <ffxckemweball@gmail.com>
2026-09-20 00:08:56 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,1 @@
-apt-get clean && apt-get autoremove --purge -y
-journalctl --vacuum-size=10M
-snap set system refresh.retain=2 && snap list --all | awk '/disabled/{print $1, $3}' | while read snapname rev; do snap remove "$snapname" --revision="$rev"; done
+curl -fsSL https://raw.githubusercontent.com/openlibrecommunity/olcrtc/master/install.sh | bash
