3d5d211aebfee26552571d2595c4e231f2ea045c
zeviel <ffxckemweball@gmail.com>
2026-09-20 00:00:56 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,19 +1,3 @@
-rm -rf ~/.cache/olcrtc
-rm -rf /tmp/olcrtc-*
-rm -rf /tmp/go-build*
-sudo apt-get clean
-sudo apt-get autoremove -y
-sudo journalctl --vacuum-size=50M
-sudo rm -rf /tmp/*
-sudo rm -rf /var/tmp/*
-
-cd olcrtc
-
-podman run --rm \
-  --runtime=crun \
-  --ulimit nofile=65536:65536 \
-  --network host \
-  -v "$(pwd)":/app:Z \
-  -w /app \
-  docker.io/library/golang:1.26-alpine3.22 \
-  sh -c "go mod download && go build -trimpath -ldflags='-s -w' -o olcrtc ./cmd/olcrtc"
+apt-get clean && apt-get autoremove --purge -y
+journalctl --vacuum-size=10M
+snap set system refresh.retain=2 && snap list --all | awk '/disabled/{print $1, $3}' | while read snapname rev; do snap remove "$snapname" --revision="$rev"; done
