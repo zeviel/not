912bc0681ffff235a2cc80b6ca189c60d79be504
zeviel <ffxckemweball@gmail.com>
2026-09-23 19:53:25 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,14 +1,10 @@
-cd /tmp/olcrtc-deploy-*   # или путь, куда скрипт склонировал
-# либо:
-cd ~/olcrtc 2>/dev/null || cd /tmp/olcrtc-*
+apt-get -o DPkg::Lock::Timeout=600 update
+apt-get -o DPkg::Lock::Timeout=600 install -y ca-certificates git
 
-ulimit -n 65536
+WPP_DIR="$(mktemp -d /root/wpp-install.XXXXXX)"
+git -c http.version=HTTP/1.1 clone --depth 1 --branch v2.3.6 \
+  https://github.com/POLESNIESOVETI12/web-panel-proxy.git "$WPP_DIR"
 
-podman run --rm \
-  --runtime=crun \
-  --ulimit nofile=65536:65536 \
-  --network host \
-  -v "$(pwd)":/app:Z \
-  -w /app \
-  docker.io/library/golang:1.26-alpine3.22 \
-  sh -c "go mod download && go build -trimpath -ldflags='-s -w' -o olcrtc ./cmd/olcrtc"
+cd "$WPP_DIR"
+chmod +x ./*.sh
+bash ./install-final.sh
