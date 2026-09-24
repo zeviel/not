a09cd232f2429d3380e91da3215e6ffceaf4293c
zeviel <ffxckemweball@gmail.com>
2026-09-24 21:19:29 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,4 +2,4 @@ wget "https://github.com/p1neappleXpress/OpenFlux/releases/download/0.0.4/openfl
 chmod +x openflux-linux-amd64
 sudo ./openflux-linux-amd64 --role=exit --mode=l4 \
     --transport=yandex \
-    --url="https://boards.yandex.ru/whiteboard/?hash=d3cf401d147809302c35d2859a4d4e05"
+    --url="https://boards.yandex.ru/whiteboard/?hash=d3cf401d147809302c35d2859a4d4e05" --debug
