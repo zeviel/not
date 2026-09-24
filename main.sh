e309c48476b97caa9764af1468e8689e2af4b954
zeviel <ffxckemweball@gmail.com>
2026-09-24 21:03:58 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 wget "https://github.com/p1neappleXpress/OpenFlux/releases/download/0.0.4/openflux-linux-arm"
 chmod +x openflux-linux-arm
-sudo ./openflux-linux-arm64 --role=exit --mode=l3 \
+sudo ./openflux-linux-arm --role=exit --mode=l3 \
     --transport=yandex \
     --url="https://boards.yandex.ru/whiteboard/?hash=d3cf401d147809302c35d2859a4d4e05"
