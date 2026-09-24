9c78b38a6e1a7bac199bb095214a2e4da6336449
zeviel <ffxckemweball@gmail.com>
2026-09-24 21:36:12 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,1 @@
-wget "https://github.com/p1neappleXpress/OpenFlux/releases/download/0.0.4/openflux-linux-amd64"
-chmod +x openflux-linux-amd64
-sudo ./openflux-linux-amd64 --role=exit --mode=l4 --transport=cupsonline --debug
+
