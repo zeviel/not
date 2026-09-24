37ae0ea806733dc4ea53ba532f5efb280926b054
zeviel <ffxckemweball@gmail.com>
2026-09-24 21:21:58 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 wget "https://github.com/p1neappleXpress/OpenFlux/releases/download/0.0.4/openflux-linux-amd64"
 chmod +x openflux-linux-amd64
 sudo ./openflux-linux-amd64 --role=exit --mode=l4 \
-    --transport=yandex \
+     --transport=vyandex \
     --url="https://boards.yandex.ru/whiteboard/?hash=d3cf401d147809302c35d2859a4d4e05" --debug
